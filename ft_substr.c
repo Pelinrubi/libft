6e48411fbb44aceddac5ead52f107da487c0f288
Alba 42 <algarci5@student.42madrid.com>
2024-09-28 21:54:36 +0200

    -m "finished substr"

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: algarci5 <algarci5@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 20:30:39 by algarci5          #+#    #+#             */
-/*   Updated: 2024/09/28 20:49:34 by algarci5         ###   ########.fr       */
+/*   Updated: 2024/09/28 21:52:20 by algarci5         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,23 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char *sub;
+	char	*sub;
+	int		size;
 
 	if (!s)
 		return (NULL);
-	if (start >)
+	if (start > len)
+		return (NULL);
+	sub = malloc((char *)*(len - start));
+	if (!sub)
+		return (NULL);
+	size = 0;
+	while (s[start])
+	{
+		sub[size] = s[start];
+		size++;
+		start++;
+	}
+	sub[size] = '\0';
+	return (sub);
 }
